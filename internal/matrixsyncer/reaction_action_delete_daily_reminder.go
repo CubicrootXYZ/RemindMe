284534b6709731b887923cbc947c16fec6d252d2
Cubicroot <github@cubicroot.xyz>
2021-09-05 15:34:29 +0200

    handle unhandled error

--- internal/matrixsyncer/reaction_action_delete_daily_reminder.go
+++ internal/matrixsyncer/reaction_action_delete_daily_reminder.go
@@ -2,6 +2,7 @@ package matrixsyncer
 
 import (
 	"github.com/CubicrootXYZ/matrix-reminder-and-calendar-bot/internal/database"
+	"github.com/CubicrootXYZ/matrix-reminder-and-calendar-bot/internal/log"
 	"maunium.net/go/mautrix/event"
 )
 
@@ -17,6 +18,11 @@ func (s *Syncer) getReactionActionDeleteDailyReminder(rat ReactionActionType) *R
 
 func (s *Syncer) reactionActionDeleteDailyReminder(message *database.Message, content *event.ReactionEventContent, evt *event.Event, channel *database.Channel) error {
 	c, err := s.daemon.Database.UpdateChannel(channel.ID, channel.TimeZone, nil)
+	if err != nil {
+		log.Error(err.Error())
+		msg := "Sorry I was not able to delete the daily reminder."
+		s.messenger.SendFormattedMessage(msg, msg, c, database.MessageTypeDailyReminderDeleteSuccess, 0)
+	}
 
 	msg := "I will no longer send you a daily message. To reactivate this feature message me with \"set daily reminder at 10:00\"."
 	_, err = s.messenger.SendFormattedMessage(msg, msg, c, database.MessageTypeDailyReminderDeleteSuccess, 0)
