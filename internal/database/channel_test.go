7923757db3639ecf45f0aaba1601d000274e9d1d
Cubicroot <github@cubicroot.xyz>
2021-10-17 19:09:33 +0200

    add testing for timezones

--- internal/database/channel_test.go
+++ internal/database/channel_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/CubicrootXYZ/matrix-reminder-and-calendar-bot/internal/roles"
@@ -8,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestChannel_AddChannel(t *testing.T) {
+func TestChannel_AddChannelOnSuccess(t *testing.T) {
 	assert := assert.New(t)
 	db, mock := testDatabase()
 
@@ -50,4 +51,57 @@ func TestChannel_AddChannel(t *testing.T) {
 	}
 
 	assert.NoError(mock.ExpectationsWereMet())
+}
+
+func TestChannel_AddChannelOnFailure(t *testing.T) {
+	assert := assert.New(t)
+	db, mock := testDatabase()
+
+	for _, channel := range testChannels() {
+
+		mock.ExpectBegin()
+		mock.ExpectExec("INSERT INTO `channels`").WithArgs(
+			sqlmock.AnyArg(),
+			sqlmock.AnyArg(),
+			sqlmock.AnyArg(),
+			sqlmock.AnyArg(),
+			channel.ChannelIdentifier,
+			channel.UserIdentifier,
+			sqlmock.AnyArg(),
+			sqlmock.AnyArg(),
+			sqlmock.AnyArg(),
+			roles.RoleUser).WillReturnError(errors.New("test error"))
+		mock.ExpectRollback()
+
+		_, err := db.AddChannel(channel.UserIdentifier, channel.ChannelIdentifier, roles.RoleUser)
+		assert.Error(err)
+	}
+
+	assert.NoError(mock.ExpectationsWereMet())
+}
+
+func TestChannel_TimezoneOnSuccess(t *testing.T) {
+	assert := assert.New(t)
+	channel := testChannel1()
+
+	zones := []string{"Europe/Berlin", "Africa/Bamako", "America/Mexico_City", "Asia/Jakarta", "Europe/Budapest", "Pacific/Fiji", "US/Samoa"}
+
+	for _, zone := range zones {
+		channel.TimeZone = zone
+
+		assert.Equalf(zone, channel.Timezone().String(), "Timezone %s returned as %s", zone, channel.Timezone().String())
+	}
+}
+
+func TestChannel_TimezoneOnFailure(t *testing.T) {
+	assert := assert.New(t)
+	channel := testChannel1()
+
+	zones := []string{"Euro/Berlin", "Africa Bamako", "Mexico City", "Asia", "now", "", "1234"}
+
+	for _, zone := range zones {
+		channel.TimeZone = zone
+
+		assert.Equalf("UTC", channel.Timezone().String(), "Timezone %s returned as %s", zone, channel.Timezone().String())
+	}
 }
