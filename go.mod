9a5496c4f539d3f036a781d36ebc0b3569f92b9f
Cubicroot <github@cubicroot.xyz>
2022-06-09 20:23:57 +0200

    allow weeks, months and years (#81)
    allow weeks, months and years

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/BurntSushi/toml v0.4.1 // indirect
-	github.com/CubicrootXYZ/gonaturalduration v0.0.0-20211017132232-aa3bbfc675a0
+	github.com/CubicrootXYZ/gonaturalduration v0.0.0-20220607181758-35e7a64965f5
 	github.com/CubicrootXYZ/gormlogger v0.0.0-20211030135540-f090b6c3590d
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/dchest/uniuri v0.0.0-20200228104902-7aecb25e1fe5
--- internal/matrixsyncer/reply_action_recurring.go
+++ internal/matrixsyncer/reply_action_recurring.go
@@ -16,7 +16,7 @@ func (s *Syncer) getReplyActionRecurring(rtt []database.MessageType) *types.Repl
 	action := &types.ReplyAction{
 		Name:         "Make a reminder recurring",
 		Examples:     []string{"every 10 days", "each twenty two hours and five seconds"},
-		Regex:        "(?i)(repeat|every|each|always|recurring|all|any).*(second|minute|day|hour)(|s)$",
+		Regex:        "(?i)(repeat|every|each|always|recurring|all|any).*(second|minute|hour|day|week|month|year)(|s)$",
 		ReplyToTypes: rtt,
 		Action:       s.replyActionRecurring,
 	}
