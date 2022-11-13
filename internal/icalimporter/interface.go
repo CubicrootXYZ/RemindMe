52c4e0717ed35a4545cdf700b60a889e9f19065a
cubicroot <github@cubicroot.xyz>
2022-11-13 18:23:42 +0100

    fix parsing rrule

--- internal/icalimporter/interface.go
+++ internal/icalimporter/interface.go
@@ -1,5 +1,12 @@
 package icalimporter
 
+import "errors"
+
+var (
+	// ErrMissingDtStart indicates that the event has no DTSTART entry
+	ErrMissingDtStart = errors.New("missing DTSTART in event")
+)
+
 // IcalImporter imports events from ical sources and sets them as proper reminders
 type IcalImporter interface {
 	Run()
