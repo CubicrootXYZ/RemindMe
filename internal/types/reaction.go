fc2a6a818b0edd0b61e402463b949b2354400a09
cubicroot <github@cubicroot.xyz>
2022-12-24 21:37:25 +0100

    reduce reactions send

--- internal/types/reaction.go
+++ internal/types/reaction.go
@@ -1,4 +1,4 @@
 package types
 
-var ReactionsReminderRequest = []string{"❌", "➕", "1️⃣", "2️⃣", "4️⃣", "6️⃣", "8️⃣", "🔟"}
-var ReactionsReminder = []string{"✅", "🔄", "➕", "1️⃣", "2️⃣", "4️⃣", "6️⃣", "🔟"}
+var ReactionsReminderRequest = []string{"❌", "➕", "1️⃣", "4️⃣"}
+var ReactionsReminder = []string{"✅", "🔄", "➕", "1️⃣", "4️⃣", "🔟"}
