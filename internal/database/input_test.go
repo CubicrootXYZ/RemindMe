40c66920cd43d6d70283181258bcbf6dd5b5aa8b
cubicroot <github@cubicroot.xyz>
2023-01-22 13:32:23 +0100

    add missing test for input

---
+++ internal/database/input_test.go
@@ -0,0 +1,25 @@
+package database_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetInputByID(t *testing.T) {
+	channel, err := service.NewChannel(testChannel())
+	require.NoError(t, err)
+
+	inputBefore := testInput()
+
+	err = service.AddInputToChannel(channel.ID, inputBefore)
+	require.NoError(t, err)
+
+	inputAfter, err := service.GetInputByID(inputBefore.ID)
+	require.NoError(t, err)
+
+	assert.Equal(t, inputBefore.ID, inputAfter.ID)
+	assert.Equal(t, inputBefore.InputID, inputAfter.InputID)
+	assert.Equal(t, inputBefore.InputType, inputAfter.InputType)
+}
