ac2bb4d98a96517dab87a6d2336ce85ce0bc3ee1
cubicroot <github@cubicroot.xyz>
2023-03-22 19:55:33 +0100

    api: test GetUintFromContext

--- internal/api/apictx/params.go
+++ internal/api/apictx/params.go
@@ -4,11 +4,15 @@ import "github.com/gin-gonic/gin"
 
 // GetUintFromContext returns the ID from the context.
 func GetUintFromContext(ctx *gin.Context, name string) (uint, bool) {
-	// TODO test
-	id, ok := ctx.MustGet(name).(uint)
+	valueRaw, ok := ctx.Get(name)
 	if !ok {
 		return 0, false
 	}
 
-	return id, true
+	value, ok := valueRaw.(uint)
+	if !ok {
+		return 0, false
+	}
+
+	return value, true
 }
